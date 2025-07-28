89717f6257ae4cc33ffb2e81f461a59f6335cf33
frynan4 <anna.helloiza.santos@escola.pr.gov.br>
2025-07-28 12:22:35 -0300

    adicionado o botão de sair (precisará de uma verificação para ver se está correto

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
- 
+ <script>
+  const sairButton = document.querySelector('.sidebar button:last-child');
+
+  sairButton.addEventListener('click', function() {
+    
+    window.location.href = './page1/index.html';
+  });
+</script>
